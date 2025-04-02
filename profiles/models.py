b7cb10e3adbed442c06ae51e71051c73b67426a7
Lewis Bull <lewisbull2303@gmail.com>
2025-04-02 16:11:56 +0100

    Added my imports to my models.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
-
-# Create your models here.
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
