5201c734cb057b5a841796186575c83138a6352c
Lewis Bull <lewisbull2303@gmail.com>
2025-04-02 16:19:59 +0100

    Added my ordering and the profile to be automatically created in a function

--- profiles/models.py
+++ profiles/models.py
@@ -18,5 +18,21 @@ class Profile(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_avatar_aiks6l'
+        upload_to='images/', default='../Default_pfp_nxyoza'
     )
+
+    class Meta:
+        ordering = ['-created_on']
+    
+    def __str__(self):
+        """
+        Return information who the profile owner is
+        """
+        return f"{self.owner}'s profile"
+    
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(owner=instance)
+
+
+    post_save.connect(create_profile, sender=User)
