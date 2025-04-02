32f43799956dc53dac6cfd7390c4e96bc953683a
Lewis Bull <lewisbull2303@gmail.com>
2025-04-02 16:47:33 +0100

    Created a view for the profile details

--- profiles/views.py
+++ profiles/views.py
@@ -10,5 +10,28 @@ from django_filters.rest_framework import DjangoFilterBackend
 # Internal:
 from .models import Profile
 from .serializers import ProfileSerializer
-
+from dripdrop.permissions import IsOwnerOrReadOnly
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    A class for the Profile Detail
+    """
+    serializer_class = ProfileSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+        ]
+    queryset = Profile.objects.annotate(
+        posts_number=Count(
+            'owner__post',
+            distinct=True
+        ),
+        followers_number=Count(
+            'owner__followed',
+            distinct=True
+            ),
+        following_number=Count(
+            'owner__following',
+            distinct=True
+        )
+    ).order_by('-created_on')
