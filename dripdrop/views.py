e9d50b0bed3df2b60485fed2a6a206f4e72e3488
Lewis Bull <lewisbull2303@gmail.com>
2025-04-02 15:55:42 +0100

    Made the profiles app

--- dripdrop/views.py
+++ dripdrop/views.py
@@ -4,17 +4,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Internal:
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
 @api_view()
 def home(request):
     return Response({
         'status': 200,
-        'message': "Hi there, welcome to SnapFood Django REST Framework API"
+        'message': "Welcome to the DripDrop Django REST Framework API"
         })
