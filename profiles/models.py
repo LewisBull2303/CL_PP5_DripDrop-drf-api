39cbb06d5d02274d9f8e9751a8151c68237a4a70
Lewis Bull <lewisbull2303@gmail.com>
2025-04-02 16:13:16 +0100

    Created my profile class model

--- profiles/models.py
+++ profiles/models.py
@@ -5,3 +5,18 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class Profile(models.Model):
+    """
+    A class for the profile model
+    User gets automatically created via
+    signal when the user creates their account
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_avatar_aiks6l'
+    )
