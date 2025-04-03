fd281a6f2c69a8098add1dc02fce741be9124382
Lewis Bull <lewisbull2303@gmail.com>
2025-04-03 10:32:13 +0100

    Added my class for my followers list

--- followers/views.py
+++ followers/views.py
@@ -9,3 +9,16 @@ from models import Followers
 from dripdrop.permissions import IsOwnerOrReadOnly
 from .serializers import FollowerSerializer
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    The class for my followers list
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
+    queryset = Followers.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
