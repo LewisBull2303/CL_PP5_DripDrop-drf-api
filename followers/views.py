e5413c2539d14540faeb2b20d0626a2bcfd8674e
Lewis Bull <lewisbull2303@gmail.com>
2025-04-03 10:36:11 +0100

    Added my class for the followers details

--- followers/views.py
+++ followers/views.py
@@ -10,7 +10,7 @@ from dripdrop.permissions import IsOwnerOrReadOnly
 from .serializers import FollowerSerializer
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-class FollowerList(generics.ListCreateAPIView):
+class FollowersList(generics.ListCreateAPIView):
     """
     The class for my followers list
     """
@@ -22,3 +22,15 @@ class FollowerList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class FollowersDetail(generics.RetrieveDestroyAPIView):
+    """
+    The class for the Followers Details
+    Allows users to see a follower or
+    unfollow another user
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+        ]
+    queryset = Followers.objects.all()
