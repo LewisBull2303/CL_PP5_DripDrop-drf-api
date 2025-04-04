8fd11551429912e1e2f82f1e40ff4a9a0eda2603
Lewis Bull <lewisbull2303@gmail.com>
2025-04-04 16:21:07 +0100

    Added my imports and categories for my clothes in the backend

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,59 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    A class for the post model
+    """
+    category_choices = [
+    ('Classic Casual', 'Classic Casual'),
+    ('Sporty (Athleisure)', 'Sporty (Athleisure)'),
+    ('Streetwear', 'Streetwear'),
+    ('Bohemian (Boho)', 'Bohemian (Boho)'),
+    ('Preppy', 'Preppy'),
+    ('Hipster', 'Hipster'),
+    ('Grunge', 'Grunge'),
+    ('Minimalist', 'Minimalist'),
+    ('Y2K Fashion', 'Y2K Fashion'),
+    ('Soft Girl', 'Soft Girl'),
+    ('VSCO Girl', 'VSCO Girl'),
+    ('Cottagecore', 'Cottagecore'),
+    ('E-Girl', 'E-Girl'),
+    ('Skater', 'Skater'),
+    ('Dark Academia', 'Dark Academia'),
+    ('Light Academia', 'Light Academia'),
+    ('Indie', 'Indie'),
+    ('Business Casual', 'Business Casual'),
+    ('Business Professional', 'Business Professional'),
+    ('Smart Casual', 'Smart Casual'),
+    ('Power Dressing', 'Power Dressing'),
+    ('Punk', 'Punk'),
+    ('Gothic', 'Gothic'),
+    ('Cyberpunk', 'Cyberpunk'),
+    ('Rockstar Chic', 'Rockstar Chic'),
+    ('Steampunk', 'Steampunk'),
+    ('Emo', 'Emo'),
+    ('Gyaru', 'Gyaru'),
+    ('Vintage', 'Vintage'),
+    ('Retro', 'Retro'),
+    ('Kawaii', 'Kawaii'),
+    ('Harajuku', 'Harajuku'),
+    ('Lolita', 'Lolita'),
+    ('K-Pop Fashion', 'K-Pop Fashion'),
+    ('Hippie', 'Hippie'),
+    ('Winter Fashion', 'Winter Fashion'),
+    ('Summer Fashion', 'Summer Fashion'),
+    ('Resort Wear', 'Resort Wear'),
+    ('Workwear (Utility Fashion)', 'Workwear (Utility Fashion)'),
+    ('Avant-Garde', 'Avant-Garde'),
+    ('Haute Couture', 'Haute Couture'),
+    ('Luxury Chic', 'Luxury Chic'),
+    ('Sci-Fi/Fantasy Fashion', 'Sci-Fi/Fantasy Fashion'),
+    ('Rave/Festival Fashion', 'Rave/Festival Fashion'),
+    ('Normcore', 'Normcore'),
+]
