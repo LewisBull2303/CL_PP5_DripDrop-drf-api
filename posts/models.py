ccd81d1e585ee49ddc666ef935b1a18e8de1e800
Lewis Bull <lewisbull2303@gmail.com>
2025-04-04 16:22:24 +0100

    defined the fields in my posts model

--- posts/models.py
+++ posts/models.py
@@ -56,4 +56,21 @@ class Post(models.Model):
     ('Sci-Fi/Fantasy Fashion', 'Sci-Fi/Fantasy Fashion'),
     ('Rave/Festival Fashion', 'Rave/Festival Fashion'),
     ('Normcore', 'Normcore'),
-]
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=50, choices=category_choices)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../default_post_umaui6',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
