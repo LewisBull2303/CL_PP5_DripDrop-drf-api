eb016811ea21af37650cea944f05216aa8595b5d
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 12:15:39 +0100

    Added an if statement to ensure that the width of the image is not to long

--- posts/serializers.py
+++ posts/serializers.py
@@ -28,3 +28,41 @@ class PostSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Your image exceeds the height limit of 4096px.'
             )
+        # Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework import serializers
+
+# Internal:
+from .models import Post
+from likes.models import Like
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    A class for a PostSerializer
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    comments_number = serializers.ReadOnlyField()
+    likes_number = serializers.ReadOnlyField()
+
+    def validate_image(self, value):
+        """
+        Validation of the uploaded image size
+        """
+        # Image height limit of 4096 px
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Your image exceeds the height limit of 4096px.'
+            )
+
+        # Image width limit of 4096 px
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Your image exceeds the width limit of 4096px.'
+            )
