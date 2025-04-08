0a73e05f4e962a72fe038d9455ce3c61e04c977c
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 16:51:41 +0100

    Created the like class to be called when a post it liked

--- likes/models.py
+++ likes/models.py
@@ -8,3 +8,10 @@ from django.contrib.auth.models import User
 # Internal:
 from posts.models import Post
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class Like(models.Model):
+    """
+    A class for the like model
+    Related to 'owner' and 'posts'
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
