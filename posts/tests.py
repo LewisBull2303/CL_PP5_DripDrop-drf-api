612d58d1cf31d5829e97713fff521c2465cd7330
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 12:03:54 +0100

    Created the serializer for the posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,13 +1,3 @@
-# Imports
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 3rd party:
-from django.db.models import Count
-from rest_framework import generics, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from django.test import TestCase
 
-# Internal:
-from .models import Post
-from .serializers import PostSerializer
-from snapfood_drf_api.permissions import IsOwnerOrReadOnly
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Create your tests here.
