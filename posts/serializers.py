47e75009dfd952b964eb69c3a4711f3fad5db6a4
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 16:10:22 +0100

    Added my fields at the bottom of the serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -77,3 +77,22 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'owner',
+            'is_owner',
+            'profile_id',
+            'profile_image',
+            'created_on',
+            'updated_on',
+            'title',
+            'description',
+            'category',
+            'image',
+            'like_id',
+            'comments_number',
+            'likes_number',
+        ]
