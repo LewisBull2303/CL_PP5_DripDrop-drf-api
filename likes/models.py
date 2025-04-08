779ff751b3fc593ecebeaa641afcf8b7475e3d7d
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 16:49:45 +0100

    Added my imports to my models

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,10 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+# Internal:
+from posts.models import Post
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
