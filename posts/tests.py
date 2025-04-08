a308576129549047c81fa02aad4dbcff608ad826
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 12:02:55 +0100

    Added my imports into my views.py file for the posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your tests here.
+# Internal:
+from .models import Post
+from .serializers import PostSerializer
+from snapfood_drf_api.permissions import IsOwnerOrReadOnly
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
