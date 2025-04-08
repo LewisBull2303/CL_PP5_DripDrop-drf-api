7e230161b7f574dfd1a365ae1a352facd51033aa
Lewis Bull <lewisbull2303@gmail.com>
2025-04-08 12:13:54 +0100

    created the post serializer class and added my fields

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,3 +6,15 @@ from rest_framework import serializers
 # Internal:
 from .models import Post
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    A class for a PostSerializer
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    comments_number = serializers.ReadOnlyField()
+    likes_number = serializers.ReadOnlyField()
