e2bcfc186d8461c79e3b2ea666f027369607f8ee
Lewis Bull <lewisbull2303@gmail.com>
2025-04-09 09:16:56 +0100

    Created my comment class

--- comments/models.py
+++ comments/models.py
@@ -8,3 +8,8 @@ from django.contrib.auth.models import User
 # Internal:
 from posts.models import Post
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class Comment(models.Model):
+    """
+    A class for the comment model
+    """
