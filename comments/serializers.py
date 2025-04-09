30991539308bc37e92065f43199f17e1be56e190
Lewis Bull <lewisbull2303@gmail.com>
2025-04-09 09:27:22 +0100

    Added my imports to my serializer for my comments

---
+++ comments/serializers.py
@@ -0,0 +1,9 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+# Internal:
+from .models import Comment
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
