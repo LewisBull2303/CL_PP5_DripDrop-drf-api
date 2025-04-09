17b67db374df9deac6fe43f130e7671295aea6a5
Lewis Bull <lewisbull2303@gmail.com>
2025-04-09 17:03:35 +0100

    Added my permission classes and filter backend to the views

--- comments/views.py
+++ comments/views.py
@@ -10,3 +10,16 @@ from dripdrop.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    A class for ClassList to view all the comments
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
+    queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
