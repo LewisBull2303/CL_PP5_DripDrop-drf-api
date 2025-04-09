8b232220f1f505fb83c47760f20e2ae1c791a891
Lewis Bull <lewisbull2303@gmail.com>
2025-04-09 08:41:48 +0100

    Added my url links to my likes url

--- likes/urls.py
+++ likes/urls.py
@@ -6,3 +6,8 @@ from django.urls import path
 # Internal:
 from likes import views
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+urlpatterns = [
+    path('likes/', views.LikeList.as_view()),
+    path('likes/<int:pk>/', views.LikeDetail.as_view()),
+]
