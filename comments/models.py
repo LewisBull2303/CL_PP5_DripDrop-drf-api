bca9067edef935e267593edd0fe5b88a3adbb338
Lewis Bull <lewisbull2303@gmail.com>
2025-04-09 09:23:05 +0100

    Added my fields for my comment class

--- comments/models.py
+++ comments/models.py
@@ -13,3 +13,14 @@ class Comment(models.Model):
     """
     A class for the comment model
     """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.content
