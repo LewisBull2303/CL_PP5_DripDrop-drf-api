1202de2d577f0685869e9ee813b17a099d349dfb
Lewis Bull <lewisbull2303@gmail.com>
2025-04-09 09:41:21 +0100

    Added my variables to my comment serializer class

--- comments/serializers.py
+++ comments/serializers.py
@@ -12,3 +12,9 @@ class CommentSerializer(serializers.ModelSerializer):
     """
     A class for a CommentSerializer
     """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_on = serializers.SerializerMethodField()
+    updated_on = serializers.SerializerMethodField()
