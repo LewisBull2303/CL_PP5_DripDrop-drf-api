f65476b1c6c6beda2a8e88edf9b95770a054ab4b
Lewis Bull <lewisbull2303@gmail.com>
2025-04-16 10:14:02 +0100

    Corrected a small indentation error in my models for the profiles

--- profiles/models.py
+++ profiles/models.py
@@ -6,11 +6,12 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
 class Profile(models.Model):
     """
     A class for the profile model
-    User gets automatically created via
-    signal when the user creates their account
+    OneToOne relationship with User model
+    Auto-created via signal when user is created
     """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=True)
@@ -18,21 +19,22 @@ class Profile(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     image = models.ImageField(
-        upload_to='images/', default='../Default_pfp_nxyoza'
+        upload_to='images/', default='../default_avatar_aiks6l'
     )
 
     class Meta:
         ordering = ['-created_on']
-    
+
     def __str__(self):
         """
         Return information who the profile owner is
         """
         return f"{self.owner}'s profile"
-    
-    def create_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(owner=instance)
 
 
-    post_save.connect(create_profile, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
